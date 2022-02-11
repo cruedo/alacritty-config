a64177669c1f3208b6ce42799623ab08254654ae
cruedo <74605828+cruedo@users.noreply.github.com>
2022-02-12 00:37:52 +0530

    Splitted themes into multiple files

--- script.py
+++ script.py
@@ -1,55 +1,56 @@
 from bs4 import BeautifulSoup as bs
 import requests as req
-import sys
-
-
-def m_write(name, cols, f):
-	bad = ['!','.','@','#','$']
-	name = ''.join(filter(lambda x: x not in bad, name))
-	cols = "\n"+cols
-	cols = cols.replace("\n","\n  ")
-	N = f"{name}: &{name}"
-	cols = cols.replace("colors:", N)
-	f.write(cols)
-
-
+from pathlib import Path
+ 
+ 
 if __name__ == "__main__":
-
-	output_file = "themes.yml"
-	if(len(sys.argv) > 1):
-		output_file = sys.argv[1]
-
-	url = "https://github.com/alacritty/alacritty/wiki/Color-schemes"
-	res = req.get(url)
-	html_doc = res.content
-
-	soup = bs(html_doc, 'html.parser')
-
-	attr = {
-		"class" : "markdown-body"
-	}
-
-	x = soup.find(attrs=attr).find_all('details')
-
-	cnt = 0
-	themes = list(x)
-	f = open(output_file, "w")
-	f.write("schemes:\n\n")
-
-	for i in themes:
-		cnt += 1
-		variants = list(i.find_all('div'))
-		name = i.summary.string.replace(" ","")
-
-		sz = len(variants)
-		rname = name
-
-		for k,v in enumerate(variants):
-			if(sz == 2):
-				if(k == 0): rname = name + '-light'
-				else: rname = name + '-dark'
-			cols = v['data-snippet-clipboard-copy-content']
-			m_write(rname, cols, f)
-
-
-	f.close()
+ 
+    HOME = str(Path.home())
+    BASE_PATH = HOME + "/.config/alacritty/themes/"
+    Path(BASE_PATH).mkdir(parents=True, exist_ok=True)
+ 
+    url = "https://github.com/alacritty/alacritty/wiki/Color-schemes"
+    res = req.get(url)
+    html_doc = res.content
+ 
+    soup = bs(html_doc, 'html.parser')
+ 
+    attr = {
+        "class" : "markdown-body"
+    }
+ 
+    x = soup.find(attrs=attr).find_all('details')
+ 
+    themes = list(x)
+    print("import:")
+ 
+    for i in themes:
+        variants = list(i.find_all('div'))
+        name = i.summary.string.replace(" ","")
+        sz = len(variants)
+        
+        for k,v in enumerate(variants):
+            variant_type = ""
+            if(sz == 2):
+                if(k == 0): variant_type = '-Light'
+                else: variant_type = '-Dark'
+            
+            theme_name = name + variant_type
+            cols = v['data-snippet-clipboard-copy-content']
+ 
+            bad = ['!','.','@','#','$']
+            theme_name = ''.join(filter(lambda x: x not in bad, theme_name))
+ 
+            theme_name = theme_name.title()
+            root_yml = f"{theme_name}: &{theme_name}"
+            cols = cols.replace("colors:", root_yml)
+            
+            theme_filename = theme_name + ".yml"
+            theme_path = BASE_PATH + theme_filename
+            
+            with open(theme_path, "w") as f:
+                f.write(cols)
+                f.write(f"\n\ncolors:\n  *{theme_name}")
+ 
+            print("#  - " + "~/.config/alacritty/themes/" + theme_filename)
+ 
